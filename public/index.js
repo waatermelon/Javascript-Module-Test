b75d31490dcf42369a707964daf8a646eda38ec6
waatermelon <hugh.takara@gmail.com>
2024-04-28 23:37:20 -0600

    updated to use files from firebase

--- public/index.js
+++ public/index.js
@@ -1,5 +1,14 @@
 import { buttonPress } from "./button.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCSF5bPC_Yd9zSD5Qd5QzdfYOvYOiVxS1A",
     authDomain: "javascript-test-5d280.firebaseapp.com",
@@ -11,6 +20,9 @@ const firebaseConfig = {
     measurementId: "G-PD5N98KXKD"
   };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 let button1 = document.getElementById('button1');
@@ -34,3 +46,32 @@ button1.onclick = () => {
 
     document.body.appendChild(newButton);
 }
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+const colRef = collection(db, 'items');
+
+// Add a document with a generated ID
+// addDoc(colRef, {
+//   name: 'Item 1',
+//   description: 'Description of Item 1',
+//   price: 10.99
+// })
+//   .then(docRef => {
+//     console.log('Document added with ID: ', docRef.id);
+//   })
+//   .catch(error => {
+//     console.error('Error adding document: ', error);
+//   });
+
+//   getDocs(colRef)
+//   .then(snapshot => {
+//     snapshot.forEach(doc => {
+//       console.log(doc.id, ' => ', doc.data());
+//     });
+//   })
+//   .catch(error => {
+//     console.error('Error getting documents: ', error);
+//   });
